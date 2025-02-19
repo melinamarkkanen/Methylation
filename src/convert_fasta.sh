eba9eeaed19050ad594db8e9adbf688837f55de2
Melina Markkanen <melina.markkanen@helsinki.fi>
2025-02-19 19:01:09 +0200

    first workflow

---
+++ src/convert_fasta.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set barcode variable
+barcode=$1
+
+# Convert to .fasta
+awk '/^S/{print ">"$2;print $3}' HAMBI_data/metagenomic_assembly/${barcode}".bp.p_ctg.gfa" > HAMBI_data/metagenomic_assembly/${barcode}".fasta"
+
+# Rename to have the sample name in fron of contig ID
+sed "s/^>/&${barcode}_/g" HAMBI_data/metagenomic_assembly/${barcode}".fasta" > HAMBI_data/metagenomic_assembly/${barcode}"_contigs.fasta"
