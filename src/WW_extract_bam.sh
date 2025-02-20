76aa27cec010d8dbfbd7bd9390c58989e4eded1e
Melina Markkanen <melina.markkanen@helsinki.fi>
2025-02-20 21:12:42 +0200

    WW workflows

---
+++ src/WW_extract_bam.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set barcode variable
+sample=$1
+
+# Load the SeqKit
+module load biokit
+
+# Set contig variable
+
+contigs=$(ls WW_data/$sample"_contigs"/*.fasta | sed 's/WW_data\/{sample}"_contigs"///g' | sed 's/\.fasta//')
+
+# Complete contigs
+for c in $contigs; do
+        echo $c > WW_data/{sample}/search
+        cat WW_data/{sample}/search | xargs samtools view -F 4 -b WW_data/{sample}/$sample"_contigs_mapped.bam" > WW_data/$sample"_contigs"/$c".bam"
+done
