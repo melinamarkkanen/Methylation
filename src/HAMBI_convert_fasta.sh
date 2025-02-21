a7a9f79ad52d6fc6d95a0b75d840047413819531
Melina Markkanen <melina.markkanen@helsinki.fi>
2025-02-21 14:46:09 +0200

    changed kinetics

--- src/HAMBI_convert_fasta.sh
+++ src/HAMBI_convert_fasta.sh
@@ -3,8 +3,7 @@
 # Set barcode variable
 barcode=$1
 
-# Convert to .fasta
-awk '/^S/{print ">"$2;print $3}' HAMBI_data/metagenomic_assembly/${barcode}".bp.p_ctg.gfa" > HAMBI_data/metagenomic_assembly/${barcode}".fasta"
+awk '/^S/{print ">"$2;print $3}' HAMBI_data/metagenomic_assembly/${barcode}".asm.bp.p_ctg.gfa" > HAMBI_data/metagenomic_assembly/${barcode}".fasta"
 
 # Rename to have the sample name in fron of contig ID
 sed "s/^>/&${barcode}_/g" HAMBI_data/metagenomic_assembly/${barcode}".fasta" > HAMBI_data/metagenomic_assembly/${barcode}"_contigs.fasta"
--- src/WW_extract_bam.sh
+++ src/WW_extract_bam.sh
@@ -4,14 +4,14 @@
 sample=$1
 
 # Load the SeqKit
-module load biokit
+module load seqkit/2.5.1
 
 # Set contig variable
 
-contigs=$(ls WW_data/$sample"_contigs"/*.fasta | sed 's/WW_data\/{sample}"_contigs"///g' | sed 's/\.fasta//')
+contigs=$(ls WW_data/$sample"_contigs"/*.fasta | sed 's/WW_data\/${sample}"_contigs"//g' | sed 's/\.fasta//')
 
 # Complete contigs
 for c in $contigs; do
-        echo $c > WW_data/{sample}/search
-        cat WW_data/{sample}/search | xargs samtools view -F 4 -b WW_data/{sample}/$sample"_contigs_mapped.bam" > WW_data/$sample"_contigs"/$c".bam"
+        echo $c > WW_data/$sample/search
+        cat WW_data/$sample/search | xargs samtools view -F 4 -b WW_data/$sample/$sample"_contigs_mapped.bam" > WW_data/$sample"_contigs"/$c".bam"
 done
